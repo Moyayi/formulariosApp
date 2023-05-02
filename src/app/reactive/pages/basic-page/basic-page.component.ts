2d1b4c2d0f3a4e5c7636b2857838d138ea75e549
Pedro <pedro.moyano.garcia@gmail.com>
2023-05-02 20:37:28 +0200

    Added reactive forms

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const product = {
+  name      : 'RTX 4090',
+  price     : 2000,
+  inStorage : 6
+}
+
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
+  
+  public myForms : FormGroup = this.fb.group({
+    name      : ['', [ Validators.required, Validators.minLength(3),  ] ],
+    price     : [0, [Validators.required, Validators.min(0)] ],
+    inStorage : [0, [Validators.required, Validators.min(0)] ],
+  })
+  constructor( private fb : FormBuilder ){}
 
+  ngOnInit(): void {
+    // this.myForms.reset( product )
+  }
+
+  isValidField( field : string ) : boolean | null{
+    return this.myForms.controls[field].errors && this.myForms.controls[field].touched
+  }
+
+  getFieldError(field : string ) : string | null {
+
+    if ( !this.myForms.controls[field] ) return null;
+
+    const errors = this.myForms.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch( key ){
+        case "required": 
+          return "Campo obligatorio"
+        
+        case "minlength":
+          return `Este campo mínimo debe de tener ${ errors['minlength'].requiredLength} de longitud` 
+      }
+    }
+
+
+    return "Hola mundo";
+  }
+
+  onSave():void{
+    if( this.myForms.invalid ){
+      this.myForms.markAsTouched();
+      return
+    };
+    console.log(this.myForms.value)
+
+    this.myForms.reset({price:0 , inStorage : 0})
+  }
 }
